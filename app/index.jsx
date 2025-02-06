77269c9544b1c462ebc34f55d737cc6063a79b56
sean <sstringfellow350@gmail.com>
2025-02-06 16:22:12 -0600

    welcome, login, and sinup

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+// import { View, Text, Button } from 'react-native';
+// import React from 'react';
+// import { useNavigation } from '@react-navigation/native';
+// import ScreenWrapper from '../components/ScreenWrapper';
+
+// const Index = () => {
+//   const navigation = useNavigation();
+//   return (
+//     <ScreenWrapper>
+//       <Text>hello</Text>
+//       <Button title="welcome" onPress={() => navigation.navigate('Welcome')} />
+//     </ScreenWrapper>
+//   );
+// };
+
+// export default Index;
---
+++ app/welcome.jsx
@@ -0,0 +1,92 @@
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
+import React from 'react';
+import ScreenWrapper from '../components/ScreenWrapper';
+import { StatusBar } from 'expo-status-bar';
+import { wp, hp } from '../helpers/common';
+import { theme } from '../constants/theme';
+import Button from '../components/Button';
+import { useNavigation } from '@react-navigation/native';
+
+const Welcome = () => {
+  const navigation = useNavigation();
+
+  return (
+    <ScreenWrapper bg="white">
+      <StatusBar style="dark" />
+      <View style={styles.container}>
+        {/*welcome image*/}
+        <Image style={styles.welcomeImage} resizeMode='contain' source={require('../assets/illustration.png')} />
+
+        {/*title*/}
+        <View style ={{gap: 10}}>
+          <Text style={styles.title}>Bragging Rightz!</Text>
+          <Text style={styles.punchline}>Bet Big, Brag Bigger</Text>
+        </View>
+        {/*footer*/}
+        <View style ={styles.footer}>
+          <Button 
+            title="Getting Started"
+            buttonStyle={{marginHorizontal: wp(3)}}
+            onPress={() => navigation.navigate('SignUp')}
+          
+          />
+          <View style={styles.bottomTextContainer}>
+            <Text style={styles.loginText}>
+              Already have an account?
+            </Text>
+            <Pressable onPress={() => navigation.navigate('Login')}>
+            <Text style={[styles.loginText, {color: theme.colors.primaryDark, fontWeight: theme.fonts.semibold}]}>
+              Login
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+
+      </View>
+    </ScreenWrapper>
+  );
+};
+
+export default Welcome;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    paddingHorizontal: wp(4),
+  },
+  welcomeImage: {
+    width: wp(125),
+    height: hp(40),
+    alignSelf: 'center'
+  },
+  title: {
+    fontSize: hp(4),
+    fontWeight: theme.fonts.extraBold,
+    color: theme.colors.text,
+    textAlign: 'center',
+  },
+  punchline: {
+    textAlign: 'center',
+    paddingHorizontal: wp(10),
+    fontSize: hp(1.7),
+    color: theme.colors.text
+  },
+  footer: {
+    gap: 30,
+    width: '100%',
+  },
+  bottomTextContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 5
+  },
+  loginText: {
+    textAlign: 'center',
+    color: theme.colors.text,
+    fontSize: hp(1.6),
+  }
+});
