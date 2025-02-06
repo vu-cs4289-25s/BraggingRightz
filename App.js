96528a71d2c585643410dfe9db76869a3b2f1df4
Sean Stringfellow <122483725+stringsc@users.noreply.github.com>
2025-02-06 17:42:14 -0600

    welcome, login, and signup (#1)
    * welcome, login, and sinup
    
    * deleted random index.jsx
    
    * fixed usernameRef

--- App.js
+++ App.js
@@ -1,89 +1,23 @@
-import 'react-native-gesture-handler';
-import React, { useEffect, useState } from 'react';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { getFirestore, doc, getDoc, collection } from 'firebase/firestore';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens';
-import LoadScreen from './src/screens/LoadScreen/LoadScreen';
-
-import { decode, encode } from 'base-64';
-if (!global.btoa) {
-  global.btoa = encode;
-}
-if (!global.atob) {
-  global.atob = decode;
-}
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Welcome from './app/welcome';
+import SignUp from './app/signUp';
+import Login from './app/login';
 
 const Stack = createStackNavigator();
-const auth = getAuth();
-const db = getFirestore();
 
 export default function App() {
-  const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        try {
-          const userDoc = doc(db, 'users', user.uid);
-          const docSnapshot = await getDoc(userDoc);
-          if (docSnapshot.exists()) {
-            const userData = docSnapshot.data();
-            setUser(userData);
-          } else {
-            setUser(null); // User document doesn't exist
-          }
-        } catch (error) {
-          console.error('Error fetching user data:', error);
-        } finally {
-          setLoading(false);
-        }
-        try {
-          const userDoc = doc(db, 'users', user.uid);
-          const docSnapshot = await getDoc(userDoc);
-          if (docSnapshot.exists()) {
-            const userData = docSnapshot.data();
-            setUser(userData);
-          } else {
-            setUser(null); // User document doesn't exist
-          }
-        } catch (error) {
-          console.error('Error fetching user data:', error);
-        } finally {
-          setLoading(false);
-        }
-      } else {
-        setUser(null); // No authenticated user
-        setLoading(false);
-        setUser(null); // No authenticated user
-        setLoading(false);
-      }
-    });
-
-    // Cleanup subscription on unmount
-    return () => unsubscribe();
-  }, []);
-
-  if (loading) {
-    return <LoadScreen />;
-  }
-
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {user ? (
-          <Stack.Screen name="Home">
-            {(props) => <HomeScreen {...props} extraData={user} />}
-          </Stack.Screen>
-        ) : (
-          <>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Registration" component={RegistrationScreen} />
-          </>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Welcome" component={Welcome} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
