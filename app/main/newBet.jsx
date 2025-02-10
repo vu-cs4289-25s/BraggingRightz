dcdb708f6c51d1c5d675696abefcbb243d9a1dbd
Sean Stringfellow <122483725+stringsc@users.noreply.github.com>
2025-02-10 15:41:01 -0600

    Seancard (#10)
    new pages

---
+++ app/main/newBet.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const NewBet = () => {
+  return (
+    <View>
+      <Text>newBet</Text>
+    </View>
+  );
+};
+
+export default NewBet;
+
+const styles = StyleSheet.create({});
