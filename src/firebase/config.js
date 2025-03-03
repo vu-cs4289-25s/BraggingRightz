1bc19ea88cf6ef235ac195ff001e02ec4322e9a5
lolitaroz <lolitarozenbaum@gmail.com>
2025-03-03 16:54:07 -0600

    env file

---
+++ src/firebase/config.js
@@ -0,0 +1,35 @@
+const AsyncStorage =
+  require('@react-native-async-storage/async-storage').default;
+const { initializeApp } = require('firebase/app');
+const {
+  getAuth,
+  initializeAuth,
+  getReactNativePersistence,
+} = require('firebase/auth');
+const { getFirestore } = require('firebase/firestore');
+const { getStorage } = require('firebase/storage');
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+export default firebaseConfig;
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Auth with AsyncStorage persistence
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+module.exports = { app, auth, db, storage };
